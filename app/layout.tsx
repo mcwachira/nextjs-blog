235c83325da04106eaf93fb4e8efbd540254143f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-01-30 02:12:31 +0300

    chore:Added the necessary files and created the Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter , Manrope} from "next/font/google";
 import "./globals.css";
+import {cx} from "@/utils";
+import Header from "@/components/ui/Header/header";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] , display:"swap", variable:"--font-sm"});
+const manrope = Manrope({ subsets: ["latin"] , display:"swap", variable:"--font-sm"});
+//By providing a value of swap , we tell the browser to render the page right away with fallback fonts, and then redraw the page once the fonts have loaded.
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+          className={cx(inter.variable,manrope.variable,'font-mr bg-light ')}>
+      <Header/>
+      {children}</body>
     </html>
   );
 }
---
+++ components/ui/Header/header.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Logo from "@/components/ui/Header/Logo";
+import { DribbbleIcon, GithubIcon, LinkedinIcon, MoonIcon, SunIcon, TwitterIcon } from "@/components/ui/icons";
+
+import Link from "next/link";
+
+function Header(props) {
+    return (
+        <header className='w-full p-4 px-10 flex items-center justify-between'>
+
+            <Logo/>
+
+            <nav
+                className=" w-max py-3 px-8 border-solid border-dark border-2 rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm ">
+                <Link href='/' className="mr-2">Home</Link>
+
+                <Link href='/' className="mx-2">Home</Link>
+
+                <Link href='/' className="mr-2">Home</Link>
+
+
+                <button>
+                    <SunIcon/>
+
+                </button>
+            </nav>
+
+            <div className=" hidden sm:flex items-center">
+                <a href="#" className="inline-block w-6 h-6 mr-4"
+                   aria-label="Reach out to me via LinkedIn" target="_blank"><LinkedinIcon
+                    className="hover:scale-125 transition-all ease duration-200"/></a>
+                <a href="#" className="inline-block w-6 h-6 mr-4"
+                   aria-label="Reach out to me via Twitter" target="_blank"><TwitterIcon
+                    className="hover:scale-125 transition-all ease duration-200"/></a>
+                <a href="#"  className="inline-block w-6 h-6 mr-4"
+                   aria-label="Check my profile on Github" target="_blank"><GithubIcon
+                    className="hover:scale-125 transition-all ease duration-200 dark:fill-light"/></a>
+                <a href="#"  className="inline-block w-6 h-6 mr-4"
+                   aria-label="Check my profile on Dribbble" target="_blank"><DribbbleIcon
+                    className="hover:scale-125 transition-all ease duration-200"/></a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
